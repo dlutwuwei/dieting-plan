5fd26808ab3c57c98a44a251f66fa088db4ffb29
wangbingkai <wangbingkai@wangbingkaideMacBook-Pro.local>
2017-02-19 22:59:29 +0800

    瘦身分享

---
+++ src/components/index/slimmingShar.vue
@@ -0,0 +1,146 @@
+<style lang="scss" scoped>
+    .slimming-share{
+        margin-top:15px;
+        .hd{
+            height: 45px;
+            line-height: 45px;
+            font-size: 18px;
+            color: #47a304;
+            background-color: #fff;
+            text-align:left;
+            .ico-1:before{
+                background-position:0 0 ;
+            }
+            .ico-1{
+                display:inline-block;
+                width:20px;
+                height:20px;
+                margin-top:-3px;
+                margin-right:3px;
+                padding-left:17px;
+                overflow:hidden;
+                vertical-align: middle;
+            }
+        }
+        .bd{
+            .share{
+                position:relative;
+                margin-bottom:13px;
+                color:#fff;
+                h5{
+                    position:absolute;
+                    margin:0;
+                    left:16px;
+                    top:14px;
+                    font-size:15px;
+                }
+                .share-info{
+                    position:absolute;
+                    left:16px;
+                    top:40px;
+                    font-size:12px;
+                    .ico{
+                        margin-top:-3px;
+                        margin-right:3px;
+                        vertical-align:middle;
+                        display:inline-block;
+                        overflow:hidden;
+                    }
+                    .ico-follow{
+                        width:12px;
+                        height:12px;
+                        margin-left:17px;
+                        overflow:hidden;
+                    }
+                    .ico-follow:before{
+                        background-position:-25px 0;
+                    }
+                    .ico-flower{
+                        width:15px;
+                        height:13px;
+                        margin-left:12px;
+                        overflow:hidden;
+                    }
+                    .ico-flower:before{
+                        background-position:-50px 0;
+                    }
+                }
+                .author-info{
+                    position:absolute;
+                    left:20px;
+                    top:159px;
+                    overflow:hidden;
+                    div:first-child{
+                        float:left;
+                        display:inline;
+                        img{
+                            display:inline-block;
+                            width:45px;
+                            height:45px;
+                            margin-left:3px;
+                            border-radius:999px;
+                            border:1px solid #fff;
+                        }
+                        p{
+                            position:relative;
+                            margin:-10px 0 0;
+                            width:50px;
+                            background:#e3c8a5;
+                            border-radius:4px;
+                            overflow: hidden;
+                            word-break: keep-all;
+                            white-space: nowrap;
+                            text-overflow: ellipsis;
+                            text-align:center;
+                            z-index:1;
+                        }
+                    }
+                    div:last-child{
+                        float:left;
+                        display:inline;
+                        margin:10px 0 0 10px;
+                        font-size:12px;
+                        line-height:1.3;
+                    }
+
+                }
+            }
+            .share-pic{
+                width:100%;
+                height:auto;
+            }
+        }
+
+    }
+</style>
+<template>
+  <div class="slimming-share">
+      <div class="hd"><span class="ico ico-1"></span>瘦身分享</div>
+      <div class="bd">
+          <div class="share" v-for="(item, index) in shareList">
+              <img :src="item.shareimg" class="share-pic" alt=""/>
+              <h5>{{item.title}}</h5>
+              <div class="share-info">
+                  {{item.time}}<span class="ico ico-follow"></span>{{item.follow}}<span class="ico ico-flower"></span>{{item.flower}}
+              </div>
+              <div class="author-info">
+                  <div>
+                      <img :src="item.authorpic" alt=""/>
+                      <p>{{item.authorname}}</p>
+                  </div>
+                  <div>
+                      {{item.authortitle}}
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
+</template>
+<script>
+    export default {
+        props: {
+            shareList: Array
+        }
+    }
+
+</script>
--- src/views/main.vue
+++ src/views/main.vue
@@ -13,17 +13,11 @@
         <mt-swipe-item class="slide3">3</mt-swipe-item>
       </mt-swipe>
     </div>
-    <card class="card">
-      <div slot="header" class="card-header">
-        <span class="title">瘦身分享</span>
-      </div>
-      <div class="share">
-        <img src="../assets/images/share1.png" alt="">
-      </div>
-      <div class="share">
-        <img src="../assets/images/share2.png" alt="">
-      </div>
-    </card>
+
+    <!--s:瘦身分享-->
+    <SlimmingShar :share-list="shareList"></SlimmingShar>
+    <!--e:瘦身分享-->
+
     <card class="card">
       <div slot="header" class="card-header">
         <span class="title">瘦身达人榜</span>
@@ -46,33 +40,41 @@
         </mt-tab-container-item>
       </mt-tab-container>
     </card>
-    <card class="card">
-      <div slot="header" class="card-header">
-        <span class="title">一分钟定制减肥计划</span>
-      </div>
-      <div v-for="n in 6" class="plan-item">
-        <div class="icon">
-          <img src="" alt="">
-        </div>
-        <div class="content">
-          <span class="title">保守型</span>
-          <span class="description">icon 是图片</span>
-        </div>
-      </div>
-    </card>
-    <mt-datetime-picker ref="picker" v-model="pickerVisible" type="date" year-format="{value} 年" month-format="{value} 月" date-format="{value} 日">
-    </mt-datetime-picker>
+    <!--e:瘦身分享-->
+    <!--e:瘦身分享-->
   </div>
 </template>
 
 <script>
   import card from '../components/card.vue';
+  import SlimmingShar from '../components/index/slimmingShar.vue';//瘦身分享
+
+  window.shareList = [{
+      "shareimg":"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1487524631615&di=48f3d53159d148514b09f4ee0e9bd1d5&imgtype=0&src=http%3A%2F%2Fpic55.nipic.com%2Ffile%2F20141206%2F13551740_235750235000_2.jpg",
+      "title":"即将踏入30岁的行列，狠狠逼自己一把",
+      "time":"2016-12-29",
+      "follow":22333,
+      "flower":1412,
+      "authorpic":"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1488119479&di=6253d2e2902471865de24c306d766c26&imgtype=jpg&er=1&src=http%3A%2F%2Fwww.qqzhi.com%2Fuploadpic%2F2014-09-14%2F194112664.jpg",
+      "authorname":"ABBBBB",
+      "authortitle":"即将踏入30岁的行列，狠狠逼自己一把"
+  },{
+      "shareimg":"https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1034097970,1628516374&fm=23&gp=0.jpg",
+      "title":"即将踏入30岁的行狠逼自己一把",
+      "time":"2016-12-29",
+      "follow":223,
+      "flower":14333,
+      "authorpic":"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1487524813632&di=5c928ed45785697000da773e614de77f&imgtype=0&src=http%3A%2F%2Fwww.gpqqtx.com%2Fuploads%2Fallimg%2F160105%2F1-160105150236.jpg",
+      "authorname":"AAAAAs",
+      "authortitle":"即将踏入30岁逼自己一把"
+  }]
 
   export default {
     data() {
       return {
         selected: '1',
-        msg: 'Use Vue 2.0 Today!'
+        msg: 'Use Vue 2.0 Today!',
+        shareList: window.shareList,
       }
     },
 
@@ -81,21 +83,31 @@
         this.$refs.picker.open();
       }
     },
-    
+
     components: {
       card,
+      SlimmingShar,
     }
   }
 
 </script>
 
 <style lang="scss">
+    .ico:before{
+        content: '';
+        display:inline-block;
+        width:150px;
+        height:150px;
+        background:url(../assets/images/ico-01.png) no-repeat;
+        background-size:100% auto;
+    }
   body {
+      background:#f7f7f7;
     font-family: Helvetica, sans-serif;
     margin: 0;
     padding: 0;
   }
-  
+
   .mint-header {
     background-color: #47a304;
   }
@@ -114,26 +126,26 @@
     max-width: 400px;
     margin: auto;
   }
-  
+
   .page-swipe {
     height: 100px;
   }
-  
+
   .page-swipe .slide1 {
     background-color: #0089dc;
     color: #fff;
   }
-  
+
   .page-swipe .slide2 {
     background-color: #ffd705;
     color: #000;
   }
-  
+
   .page-swipe .slide3 {
     background-color: #ff2d4b;
     color: #fff;
   }
-  
+
   .page-swipe-desc {
     text-align: center;
     color: #666;
