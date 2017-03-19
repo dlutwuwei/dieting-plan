c8080fa7a8cda419c08de0117e4594807acf2c6f
wuwei <wuwei@bytedance.com>
2017-03-19 17:10:54 +0800

    fix url
    Change-Id: I0cb78dace7bc4fe8236c8b97047708396ebb497c

--- src/views/plan-diet.vue
+++ src/views/plan-diet.vue
@@ -65,7 +65,7 @@
                 this.type = this.$route.params.type;
                 if (this.type != 'sport') {
                     //早中晚 type: breakfast, lunch, dinners
-                    this.$http.get(`/datefood/time/${this.date}`).then(res => {
+                    this.$http.get(`/plan/datefood/time/${this.date}`).then(res => {
                         let list = res.body[this.date][this.type]
                         list.pop(); //去掉总卡路里数
                         this.data = list;
