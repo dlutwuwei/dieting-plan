c6f8a7166587cfee3fd141247f41f8f0d0d81e7d
wangbingkai <wangbingkai@wangbingkaideMacBook-Pro.local>
2017-04-06 22:26:40 +0800

    日历页改为rem

--- src/views/plan-calendar.vue
+++ src/views/plan-calendar.vue
@@ -1,123 +1,105 @@
 <style lang="scss" scoped>
     .input-weight {
         background: #f7f7f7;
-        .hd {
-            position: relative;
-            height: 60px;
-            text-align: center;
-            font-size: 18px;
-            color: #333;
-            line-height: 60px;
-            span {
-                vertical-align: middle;
-                height: 20px;
-                width: 20px;
-                display: inline-block;
-                margin:0 auto;
-                border-bottom: 2px solid #999;
-                border-right: 2px solid #999;
-                transform: rotate(-45deg) scale(0.5);
-                -webkit-transform: rotate(-45deg) scale(0.5);
-            }
-            .lt-btn {
-                transform: rotate(135deg) scale(0.5);
-                -webkit-transform: rotate(135deg) scale(0.5);
-            }
-            em{
-                vertical-align: middle;
-                height: 35px;
-                width: 35px;
-                display: inline-block;
-                line-height:30px;
-            }
-        }
-        .hd:before {
-            content: " ";
-            position: absolute;
-            background-color: #e6e6e6;
-            bottom: -0.2rem;
-            left: 0;
-            right: 0;
-            width: 100%;
-            height: 1px;
-            -webkit-transform: scaleY(0.5);
-            transform: scaleY(0.5);
-        }
-        .bd {
-            .bd-hd {
-                padding-top: 10px;
-            }
-            .bd-hd,
-            li {
-                display: box;
-                display: -webkit-box;
-                -webkit-box-align: center;
-                box-align: center;
-                text-align: center;
-                -webkit-box-pack: justify;
-                box-pack: justify;
-                font-size: 15px;
-                color: #333;
-                position: relative;
-                padding:3px;
-                span {
-                    box-flex: 1;
-                    -webkit-box-flex: 1;
-                    display: block;
-                    text-align: center;
-                    height: 55px;
-                    padding-top: 12px;
-                    line-height: 1.2;
-                }
-                .gray {
-                    color: #999;
-                }
-                em {
-                    display: block;
-                    width:8px;
-                    height:8px;
-                    margin:0 auto;
-                    font-size: 0px;
-                    background: #47a304;
-                    border-radius:9999px;
-                }
-                .curr {
-                    background: #f2901e;
-                    border-radius: 9999px;
-                    em {
-                        background: #fff;
-                    }
-                }
-            }
-            li:before {
-                content: " ";
-                position: absolute;
-                background-color: #e6e6e6;
-                bottom: 1px;
-                left: 0;
-                right: 0;
-                width: 100%;
-                height: 1px;
-                -webkit-transform: scaleY(0.5);
-                transform: scaleY(0.5);
-            }
-        }
+    .hd {
+        position: relative;
+        height: 60px;
+        text-align: center;
+        font-size: 18px;
+        color: #333;
+        line-height: 60px;
+    span {
+        vertical-align: middle;
+        height: 20px;
+        width: 20px;
+        display: inline-block;
+        margin:0 auto;
+        border-bottom: 2px solid #999;
+        border-right: 2px solid #999;
+        transform: rotate(-45deg) scale(0.5);
+        -webkit-transform: rotate(-45deg) scale(0.5);
     }
-    @media screen and (max-width: 414px) {
-        .input-weight .bd li span{
-            height:57px;
-        }
+    .lt-btn {
+        transform: rotate(135deg) scale(0.5);
+        -webkit-transform: rotate(135deg) scale(0.5);
     }
-    @media screen and (max-width: 414px) {
-        .input-weight .bd li span{
-            height:55px;
-        }
+    em{
+        vertical-align: middle;
+        height: 35px;
+        width: 35px;
+        display: inline-block;
+        line-height:30px;
     }
-    @media screen and (max-width: 320px) {
-        .input-weight .bd li span{
-            height:46px;
-            padding-top:8px;
-        }
+    }
+    .hd:before {
+        content: " ";
+        position: absolute;
+        background-color: #e6e6e6;
+        bottom: -0.2rem;
+        left: 0;
+        right: 0;
+        width: 100%;
+        height: 1px;
+        -webkit-transform: scaleY(0.5);
+        transform: scaleY(0.5);
+    }
+    .bd {
+    .bd-hd {
+        padding-top: 10px;
+    }
+    .bd-hd,
+    li {
+        display: box;
+        display: -webkit-box;
+        -webkit-box-align: center;
+        box-align: center;
+        text-align: center;
+        -webkit-box-pack: justify;
+        box-pack: justify;
+        font-size: .3rem;
+        color: #333;
+        position: relative;
+        padding:.03rem;
+    span {
+        box-flex: 1;
+        -webkit-box-flex: 1;
+        display: block;
+        text-align: center;
+        width:1.07rem;
+        height: .9rem;
+        padding-top: .24rem;
+        line-height: 1.2;
+    }
+    .gray {
+        color: #999;
+    }
+    em {
+        display: block;
+        font-style: normal;
+        font-size: .14rem;
+        color: #47a304;
+    }
+    .curr {
+        background: #f2901e;
+        border-radius: 9999px;
+    em {
+        color: #fff;
+    }
+    }
+    }
+    li:before {
+        content: " ";
+        position: absolute;
+        background-color: #e6e6e6;
+        bottom: 1px;
+        left: 0;
+        right: 0;
+        width: 100%;
+        height: 1px;
+        -webkit-transform: scaleY(0.5);
+        transform: scaleY(0.5);
+    }
+    }
     }
 </style>
 <template>
@@ -148,6 +130,7 @@
     import $ from 'webpack-zepto';
     import {fmtDate} from  "../libs/utils.js";
     import { MessageBox } from 'mint-ui';
+    require('../libs/flexible.js');
 
     export default {
         data() {
