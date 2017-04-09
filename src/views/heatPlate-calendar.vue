3ed8779cf11ef326b4fb93b4959aaebfa10bfb9b
wuwei <wuwei@bytedance.com>
2017-04-09 23:43:28 +0800

    fix

--- src/views/heatPlate-calendar.vue
+++ src/views/heatPlate-calendar.vue
@@ -1,125 +1,109 @@
 <style lang="scss" scoped>
-    .heat-plate{
+    .heat-plate {
         margin-top: 0;
     }
     .input-weight {
         background: #f7f7f7;
-        .hd {
-            position: relative;
-            height: 60px;
-            text-align: center;
-            font-size: 18px;
-            color: #333;
-            line-height: 60px;
-            span {
-                vertical-align: middle;
-                height: 20px;
-                width: 20px;
-                display: inline-block;
-                margin:0 auto;
-                border-bottom: 2px solid #999;
-                border-right: 2px solid #999;
-                transform: rotate(-45deg) scale(0.5);
-                -webkit-transform: rotate(-45deg) scale(0.5);
-            }
-            .lt-btn {
-                transform: rotate(135deg) scale(0.5);
-                -webkit-transform: rotate(135deg) scale(0.5);
-            }
-            em{
-                vertical-align: middle;
-                height: 35px;
-                width: 35px;
-                display: inline-block;
-                line-height:30px;
-            }
-        }
-        .hd:before {
-            content: " ";
-            position: absolute;
-            background-color: #e6e6e6;
-            bottom: -0.2rem;
-            left: 0;
-            right: 0;
-            width: 100%;
-            height: 1px;
-            -webkit-transform: scaleY(0.5);
-            transform: scaleY(0.5);
-        }
-        .bd {
-            .bd-hd {
-                padding-top: 10px;
-            }
-            .bd-hd,
-            li {
-                display: box;
-                display: -webkit-box;
-                -webkit-box-align: center;
-                box-align: center;
-                text-align: center;
-                -webkit-box-pack: justify;
-                box-pack: justify;
-                font-size: .3rem;
-                color: #333;
-                position: relative;
-                padding:.03rem;
-                span {
-                    box-flex: 1;
-                    -webkit-box-flex: 1;
-                    display: block;
-                    text-align: center;
-                    width:1.07rem;
-                    height: .9rem;
-                    padding-top: .24rem;
-                    line-height: 1.2;
-                }
-                .gray {
-                    color: #999;
-                }
-                em {
-                    display: block;
-                    font-style: normal;
-                    font-size: .14rem;
-                    color: #47a304;
-                }
-                .curr {
-                    background: #f2901e;
-                    border-radius: 9999px;
-                    em {
-                        color: #fff;
-                    }
-                }
-            }
-            li:before {
-                content: " ";
-                position: absolute;
-                background-color: #e6e6e6;
-                bottom: 1px;
-                left: 0;
-                right: 0;
-                width: 100%;
-                height: 1px;
-                -webkit-transform: scaleY(0.5);
-                transform: scaleY(0.5);
-            }
-        }
+    .hd {
+        position: relative;
+        height: 60px;
+        text-align: center;
+        font-size: 18px;
+        color: #333;
+        line-height: 60px;
+    span {
+        vertical-align: middle;
+        height: 20px;
+        width: 20px;
+        display: inline-block;
+        margin:0 auto;
+        border-bottom: 2px solid #999;
+        border-right: 2px solid #999;
+        transform: rotate(-45deg) scale(0.5);
+        -webkit-transform: rotate(-45deg) scale(0.5);
     }
-    /*@media screen and (max-width: 414px) {
-        .input-weight .bd li span{
-            height:57px;
-        }
+    .lt-btn {
+        transform: rotate(135deg) scale(0.5);
+        -webkit-transform: rotate(135deg) scale(0.5);
     }
-    @media screen and (max-width: 414px) {
-        .input-weight .bd li span{
-            height:55px;
-        }
+    em{
+        vertical-align: middle;
+        height: 35px;
+        width: 35px;
+        display: inline-block;
+        line-height:30px;
     }
-    @media screen and (max-width: 320px) {
-        .input-weight .bd li span{
-            height:46px;
-            padding-top:8px;
-        }
-    }*/
+    }
+    .hd:before {
+        content: " ";
+        position: absolute;
+        background-color: #e6e6e6;
+        bottom: -0.2rem;
+        left: 0;
+        right: 0;
+        width: 100%;
+        height: 1px;
+        -webkit-transform: scaleY(0.5);
+        transform: scaleY(0.5);
+    }
+    .bd {
+    .bd-hd {
+        padding-top: 10px;
+    }
+    .bd-hd,
+    li {
+        display: box;
+        display: -webkit-box;
+        -webkit-box-align: center;
+        box-align: center;
+        text-align: center;
+        -webkit-box-pack: justify;
+        box-pack: justify;
+        font-size: .3rem;
+        color: #333;
+        position: relative;
+        padding:.03rem;
+    span {
+        box-flex: 1;
+        -webkit-box-flex: 1;
+        display: block;
+        text-align: center;
+        width:1.07rem;
+        height: .9rem;
+        padding-top: .24rem;
+        line-height: 1.2;
+    }
+    .gray {
+        color: #999;
+    }
+    em {
+        display: block;
+        font-style: normal;
+        font-size: .14rem;
+        color: #47a304;
+    }
+    .curr {
+        background: #f2901e;
+        border-radius: 9999px;
+    em {
+        color: #fff;
+    }
+    }
+    }
+    li:before {
+        content: " ";
+        position: absolute;
+        background-color: #e6e6e6;
+        bottom: 1px;
+        left: 0;
+        right: 0;
+        width: 100%;
+        height: 1px;
+        -webkit-transform: scaleY(0.5);
+        transform: scaleY(0.5);
+    }
+    }
+    }
 </style>
 <template>
     <div>
@@ -138,7 +122,7 @@
                 <ul>
                     <li v-for="(i, l) in dateList.length/7">
                         <span @click="onPopup(item)" v-for="(item, index) in dateList.slice(l*7, l*7+7)" :class="{'gray': !item.currentMonth, 'curr': (item.date==now.getDate()&&now.getMonth()==new Date().getMonth()&&item.currentMonth)}">
-                            {{item.date}}<em v-if="item.value">{{item.value}}</em>
+                            {{item.date}}<em v-if="item.value"></em>
                         </span>
                     </li>
                 </ul>
