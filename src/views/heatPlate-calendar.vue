e6f074013c63e2f1497f1459890c4c520e9e3c8b
dlutwuwei <dlutwuwei@qq.com>
2017-04-09 14:50:07 +0800

    update heatPlate-calendar.vue

--- src/views/heatPlate-calendar.vue
+++ src/views/heatPlate-calendar.vue
@@ -120,13 +120,13 @@
 </style>
 <template>
     <div>
-        <HeatPlate :heat-plate="heatPlate" :user-type="userInfo.type" :hide-title="true"></HeatPlate>
         <div class="input-weight">
             <mt-header title="鸣鹿健康">
                 <a href="/" slot="left">
                     <mt-button icon="back" @click="goback">返回</mt-button>
                 </a>
             </mt-header>
+            <HeatPlate :heat-plate="heatPlate" :user-type="userInfo.type" :hide-title="true"></HeatPlate>
             <div class="hd"><em class="lt-hander" @click="prevMonth"><span class="lt-btn"></span></em>{{now.getFullYear()}}年{{now.getMonth() + 1}}月<em class="rt-hander" @click="nextMonth"><span class="rt-btn"></span></em></div>
             <div class="bd">
                 <div class="bd-hd">
