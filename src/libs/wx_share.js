be1c5bed65236106f737ac20f9ab22228da47bc8
wangbingkai <wangbingkai@wangbingkaideMacBook-Pro.local>
2017-04-12 22:38:52 +0800

    share-test-1

--- src/libs/wx_share.js
+++ src/libs/wx_share.js
@@ -7,7 +7,7 @@ function WxShare() {
     this.shareData = {
         "title": '鸣鹿健康',// 分享标题
         "desc": '鸣鹿健康，减肥伙伴',// 分享描述
-        "link": location.href,
+        "link": encodeURIComponent(location.href) + '#/edit',
         //"link": location.href.split('#')[0], // 分享链接
         //"link": location.href.replace('#', '&'), // 分享链接
         "img_url": 'https://a1.nicaifu.com/dora/201701/ed587c92d6f09f4_ojv93q.jpg',
@@ -23,7 +23,7 @@ WxShare.prototype.init = function (config) {
         "appId": config.appId,
         "nonceStr": config.nonceStr,
         "timestamp": config.timestamp,
-        "url": encodeURIComponent(location.href.split('#')[0]),
+        //"url": encodeURIComponent(location.href.split('#')[0]),
         "signature": config.signature,
         "jsApiList": [
             'onMenuShareTimeline',
