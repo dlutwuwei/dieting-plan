d9e026b8cff10de3489f18e09154724fd2d7351a
wangbingkai <wangbingkai@wangbingkaideMacBook-Pro.local>
2017-04-15 02:19:35 +0800

    share-4-15-9

--- src/libs/wx_share.js
+++ src/libs/wx_share.js
@@ -6,7 +6,7 @@ function WxShare() {
         "title": '鸣鹿健康',// 分享标题
         "desc": '鸣鹿健康，减肥伙伴',// 分享描述
         //"link": location.href.split('#')[0] +'#'+ location.href.split('#')[1],
-        //"link": location.href, // 分享链接
+        "link": '', // 分享链接
         //"link": location.href.replace('#', '&'), // 分享链接
         "img_url": 'https://a1.nicaifu.com/dora/201701/ed587c92d6f09f4_ojv93q.jpg',
     };
