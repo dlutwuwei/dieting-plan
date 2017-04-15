a3a6f3620cf2e14f47d608e18152167521390e83
wangbingkai <wangbingkai@wangbingkaideMacBook-Pro.local>
2017-04-15 13:26:57 +0800

    share-4-15-23

--- src/components/index/sliderInfo.vue
+++ src/components/index/sliderInfo.vue
@@ -64,6 +64,18 @@
                 sliderInfo: {},
             }
         },
+        beforeCreate: function(){
+            var shareData = {
+                "title": '鸣鹿健康',// 分享标题
+                "desc": '鸣鹿健康，减肥伙伴',// 分享描述
+                "link": location.href, // 分享链接
+                "img_url": 'https://a1.nicaifu.com/dora/201701/ed587c92d6f09f4_ojv93q.jpg',
+            }
+
+
+            var wxShare = new WxShare();
+            wxShare.start(shareData);
+        },
         created() {
             this.cid = getQuery('cid');
 
