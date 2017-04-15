d02d8b8af4daa0a72c838029b1f154db3643eae1
wangbingkai <wangbingkai@wangbingkaideMacBook-Pro.local>
2017-04-15 14:20:59 +0800

    share-4-15-31

--- src/loading.js
+++ src/loading.js
@@ -15,7 +15,7 @@ import WxShare from './libs/wx_share.js';
 
 var shareData = {
     "title": '鸣鹿健康',// 分享标题
-    "desc": '鸣鹿健康，减肥伙伴',// 分享描述
+    "desc": '鸣鹿健康，瘦身伙伴',// 分享描述
     "link": location.href, // 分享链接
     "img_url": 'https://a1.nicaifu.com/dora/201701/ed587c92d6f09f4_ojv93q.jpg',
 }
