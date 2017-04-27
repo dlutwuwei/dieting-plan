ed0f0c081c1a0e0af521d0d07540e1281b9220aa
dlutwuwei <dlutwuwei@qq.com>
2017-04-27 17:09:17 +0800

    update prefer.vue

--- src/views/prefer.vue
+++ src/views/prefer.vue
@@ -195,10 +195,7 @@
             '$route': 'fetchData'
         },
         mounted() {
-            // 进入/prefer/prefer偏好首页，删除所有localstorage
-            if(!this.$route.params.type) {
-                localStorage.clear();
-            }
+ 
             this.type = this.$route.params.type || 'breakfast';
             let data = window.data[this.type];
             if(this.type==='reason') {
@@ -223,6 +220,10 @@
                 reason: '肥胖原因'
             };
             this.type = this.type || 'breakfast';
+            // 进入/prefer/prefer偏好首页，删除所有localstorage
+            if(!this.$route.params.type) {
+                localStorage.clear();
+            }
             localStorage.setItem('lumingdefault', JSON.stringify(window.data));
 
         }
